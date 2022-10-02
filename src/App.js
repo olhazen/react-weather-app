86badbd9d084e9580256ec9e9e9d264c29d53d67
olhazen <107193525+olhazen@users.noreply.github.com>
2022-10-02 16:28:34 +0100

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return ( 
+  <div className="App">
+    <h1> 
+      Weather App
+    </h1>
+    <footer>
+      This project is created by {" "}
+      <a href="https://olhasi.wixsite.com/my-site" 
+      target="_blank">
+       Olha Zen  </a> {" "} and is{" "}
+  <a href="https://github.com/olhazen/react-weather-app" target="_blank">open-sourced on GitHub </a>
+  </footer>
+  </div>
+  );
 }
