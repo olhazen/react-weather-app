311f3771311ad5c16d806770c5768fe9e061f86b
olhazen <107193525+olhazen@users.noreply.github.com>
2022-10-03 14:31:33 +0100

    Added Axios and the date on the weather component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-
+import React from 'react';
+import Weather from './Weather';
 import './App.css';
+
 
 export default function App() {
   return ( 
   <div className="App">
     <div className="container">
-    <h1> 
-      Weather App
-    </h1>
+    <Weather />
     <footer>
       This project is created by {" "}
       <a href="https://olhasi.wixsite.com/my-site" 
