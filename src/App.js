8845cf58b8246cbb7a77c40ccd73a1c2a60c6abf
olhazen <107193525+olhazen@users.noreply.github.com>
2022-10-03 16:14:01 +0100

    Edited Components

--- src/App.js
+++ src/App.js
@@ -7,13 +7,18 @@ export default function App() {
   return ( 
   <div className="App">
     <div className="container">
-    <Weather />
+    <Weather defaultCity="New York" />
+
     <footer>
-      This project is created by {" "}
+      This project was coded by{" "}
       <a href="https://olhasi.wixsite.com/my-site" 
       target="_blank">
-       Olha Zen  </a> {" "} and is{" "}
-  <a href="https://github.com/olhazen/react-weather-app" target="_blank">open-sourced on GitHub </a>
+       Olha Zen  
+       </a> {" "} 
+       and is{" "}
+  <a href="https://github.com/olhazen/react-weather-app" target="_blank">open-sourced on GitHub </a> and is{" "}
+  <a href="https://quiet-babka-4e0e23.netlify.app/" target="_blank">hosted on Netlify 
+  </a>
   </footer>
   </div>
   </div>
